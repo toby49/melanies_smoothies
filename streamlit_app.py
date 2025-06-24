0c2cb8f02985276c9250ed41b5dac60ff41d7986
toby49 <147813812+toby49@users.noreply.github.com>
2025-06-24 14:57:06 +0100

    Update streamlit_app.py
    added api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
@@ -22,6 +23,9 @@ ingredients_list = st.multiselect(
     , my_dataframe
     , max_selections = 5
 )
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 if ingredients_list:
    
